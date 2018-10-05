55195f35ade8203117b026912722c7af2b6c1262
Bui Quang Toan <bqt1089@gmail.com>
2018-10-05 17:01:38 +0700

    Update exercise Java for tester Basic

--- src/topic_12_javafortester/topic_12_JavaForTester_Exercise.java
+++ src/topic_12_javafortester/topic_12_JavaForTester_Exercise.java
@@ -30,51 +30,52 @@ public class topic_12_JavaForTester_Exercise {
 				break;
 			}
 		}
-		
+
 		System.out.println("===========Kiem tra chuoi co bat dau bang  \"Testing\" hay khong ===============");
 		String firstIndexOfText = arrayText[0];
 		System.out.println(firstIndexOfText);
 		boolean indexText = firstIndexOfText.equals("Automation");
-		
-		if (indexText == true ) {
+
+		if (indexText == true) {
 			System.out.println("Chuoi bat dau bang Automation = " + indexText);
-		}else {
+		} else {
 			System.out.println("Wrong - Chuoi bi sai");
 		}
-		
+
 		System.out.println("===========Kiem tra chuoi co ket thuc bang  \"Online\" hay khong ===============");
-//		int lastIndex = arrayText.length-1;
-//		String lastTextOfLastIndex = arrayText[lastIndex];
-//		System.out.println(lastTextOfLastIndex);
-//		boolean compareText = lastTextOfLastIndex.equals("Online");
-		
-//		if (compareText == true) {
-//			System.out.println("Text of last index = " +  lastTextOfLastIndex+ " Last text is " + compareText);
-//		} else {
-//			System.out.println("Text of last index = " +  lastTextOfLastIndex+ " Last text is " + compareText);
-//		}
-		
+		// int lastIndex = arrayText.length-1;
+		// String lastTextOfLastIndex = arrayText[lastIndex];
+		// System.out.println(lastTextOfLastIndex);
+		// boolean compareText = lastTextOfLastIndex.equals("Online");
+
+		// if (compareText == true) {
+		// System.out.println("Text of last index = " + lastTextOfLastIndex+ " Last text
+		// is " + compareText);
+		// } else {
+		// System.out.println("Text of last index = " + lastTextOfLastIndex+ " Last text
+		// is " + compareText);
+		// }
+
 		boolean compareNew = text.endsWith("Online");
-		System.out.println("Chuoi ket thuc bang Online = " +compareNew);
-		
+		System.out.println("Chuoi ket thuc bang Online = " + compareNew);
+
 		System.out.println("=========== Kiem tra vi tri cua Tutorial trong chuoi ===============");
 		int indexTut = text.indexOf("Tutorial");
 		System.out.println("Vi tri cua Tutirial trong chuoi = " + indexTut);
-		
+
 		System.out.println("=========== Thay the Online bang Offline ===============");
 		String replaceText = text.replace("Online", "Offline");
 		System.out.println("New text = " + replaceText);
-		
+
 		System.out.println("=========== Dem bao nhieu so xuat hien trong chuoi ===============");
 		int countNum = 0;
-		for (int i =0; i< arrayChar.length ; i++) {
+		for (int i = 0; i < arrayChar.length; i++) {
 			boolean verifyNum = Character.isDigit(text.charAt(i));
 			if (verifyNum == true) {
 				countNum++;
 			}
-		} 
-		System.out.println("So ky tu la so trong chuoi = "+ countNum);
+		}
+		System.out.println("So ky tu la so trong chuoi = " + countNum);
 
-		
 	}
 }
