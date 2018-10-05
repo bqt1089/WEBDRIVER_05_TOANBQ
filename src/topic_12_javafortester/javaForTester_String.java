a9a82718944a2462471339d99d8440bf08335955
Bui Quang Toan <bqt1089@gmail.com>
2018-10-05 17:00:50 +0700

    Finish exercise topic 12 JavaforTester Basic

--- src/topic_12_javafortester/javaForTester_String.java
+++ src/topic_12_javafortester/javaForTester_String.java
@@ -1,5 +1,104 @@
 package topic_12_javafortester;
 
 public class javaForTester_String {
+	public static void main(String[] args) {
+		String automation = "Automation Testing";
+		String chuoi = "Automation Tutorial";
+		String text;
 
+		/* ==========LENGTH=========== */
+		System.out.println("============= length Chieu dai chuoi===============");
+
+		int length = automation.length();
+		System.out.println("So ki tu = " + length);
+
+		/* ==========CHARTAT=========== */
+		System.out.println("============= charAt  Vi tri ki tu===============");
+
+		char kitu = chuoi.charAt(0);
+		System.out.println("Ki tu 0 = " + kitu);
+
+		/* ==========CONCATE=========== */
+		System.out.println("=============== concate Noi chuoi=============");
+		String chuoiNoi = chuoi.concat(" Test noi them");
+		System.out.println("Chuoi noi la : " + chuoiNoi);
+
+		/* ==========COMPARE=========== */
+		System.out.println("=============== Compare String =============");
+		boolean compareValue = automation.equals(chuoi);
+		System.out.println("So sanh 2 chuoi String = " + compareValue);
+
+		compareValue = automation.equals(automation);
+		System.out.println("So sanh 2 chuoi String = " + compareValue);
+
+		/* ==========INDEX=========== */
+		System.out.println("=============== indexOf Vi tri cua tu trong chuoi =============");
+		int index = automation.indexOf("Testing");
+		System.out.println("Vi tri cua tu \"Testing\" trong chuoi = " + index);
+
+		/* ==========SUBSTRING=========== */
+		System.out.println("=============== subString Chuoi con tu vi tri =============");
+		String subStringStart = automation.substring(index);
+		System.out.println("Chuoi con tu vi tri index = " + subStringStart);
+
+		String subStringEnd = automation.substring(11, 18);
+		System.out.println("Chuoi con bat dau tai 11 va ket thuc tai 18  = " + subStringEnd);
+
+		/* ==========REPLACE=========== */
+		System.out.println("=============== replace Thay the 1 tu/chuoi bang 1 tu/chuoi khac =============");
+		String money = "$100";
+		money = money.replace("$", "");
+
+		System.out.println("So sau khi da thay the = " + money);
+
+		double newMoney = Double.parseDouble(money);
+		System.out.println("So sau khi da doi qua kieu INT by Double = " + newMoney);
+
+		/* ==========SPLIT=========== */
+		System.out.println("=============== split Tach chuoi=============");
+		text = "Viewing 72 of 1827 results";
+
+		String[] subString = text.split(" ");
+		for (int i = 0; i < subString.length; i++) {
+			System.out.println("Vi tri thu " + i + ": " + subString[i]);
+		}
+
+		System.out.println("Cat chuoi va chuoi qua kieu int vi tri 1 = " + extractNumberFromString(text, 1));
+		System.out.println("Cat chuoi va chuoi qua kieu int vi tri 3 = " + extractNumberFromString(text, 3));
+
+		/* ==========UPPER - LOWER=========== */
+		System.out.println("=============== upper and lower case IN hoa IN thuong  =============");
+		String upper = automation.toUpperCase();
+		String lower = automation.toLowerCase();
+
+		System.out.println("Upper case : " + upper);
+		System.out.println("Lower case : " + lower);
+
+		/* ========== TRIM =========== */
+		System.out.println("=============== Trim khoang trang 2 dau =============");
+		String textKhoangTrang = " \t  \n  Automation Testing  \n";
+
+		System.out.println(textKhoangTrang);
+
+		String trim = textKhoangTrang.trim();
+		System.out.println("Ket qua sau khi trim = " + trim);
+
+		/* ========== Convert String / Int to Int / String =========== */
+		System.out.println("=============== Convert String / Int to Int / String =============");
+		String string = "1000";
+		int intT = 5000;
+
+		int convertFromString = Integer.parseInt(string);
+		String convertFromInt = String.valueOf(intT);
+
+		System.out.println("Value String after convert to Int = " + convertFromString);
+		System.out.println("Value Number after convert to String = " + convertFromInt);
+
+	}
+
+	public static int extractNumberFromString(String text, int number) {
+		String[] splitString = text.split(" ");
+		int result = Integer.parseInt(splitString[number]);
+		return result;
+	}
 }
---
+++ src/topic_12_javafortester/topic_12_JavaForTester_Exercise.java
@@ -0,0 +1,80 @@
+package topic_12_javafortester;
+
+import org.junit.Assert;
+
+public class topic_12_JavaForTester_Exercise {
+
+	public static void main(String[] args) {
+		String text = "Automation Testing Tutorial Online 123456";
+		int y = 0;
+
+		System.out.println("===========Dem so ky tu \"a\" trong chuoi ===============");
+		String[] arrayChar = text.split("");
+
+		for (int i = 0; i < arrayChar.length; i++) {
+			if (arrayChar[i].toLowerCase().equals("a")) {
+				y++;
+			}
+			if (i == arrayChar.length - 1) {
+				System.out.println("So lan ky tu \"a\" xuat hien la = " + y);
+			}
+		}
+
+		System.out.println("===========Kiem tra \"Testing\" co xuat hien trong chuoi hay khong ===============");
+		String[] arrayText = text.split(" ");
+
+		for (String elementInArrayText : arrayText) {
+			boolean result = elementInArrayText.equals("Testing");
+			if (result == true) {
+				System.out.println("Chuoi co chua text \"Testing\" = " + result);
+				break;
+			}
+		}
+		
+		System.out.println("===========Kiem tra chuoi co bat dau bang  \"Testing\" hay khong ===============");
+		String firstIndexOfText = arrayText[0];
+		System.out.println(firstIndexOfText);
+		boolean indexText = firstIndexOfText.equals("Automation");
+		
+		if (indexText == true ) {
+			System.out.println("Chuoi bat dau bang Automation = " + indexText);
+		}else {
+			System.out.println("Wrong - Chuoi bi sai");
+		}
+		
+		System.out.println("===========Kiem tra chuoi co ket thuc bang  \"Online\" hay khong ===============");
+//		int lastIndex = arrayText.length-1;
+//		String lastTextOfLastIndex = arrayText[lastIndex];
+//		System.out.println(lastTextOfLastIndex);
+//		boolean compareText = lastTextOfLastIndex.equals("Online");
+		
+//		if (compareText == true) {
+//			System.out.println("Text of last index = " +  lastTextOfLastIndex+ " Last text is " + compareText);
+//		} else {
+//			System.out.println("Text of last index = " +  lastTextOfLastIndex+ " Last text is " + compareText);
+//		}
+		
+		boolean compareNew = text.endsWith("Online");
+		System.out.println("Chuoi ket thuc bang Online = " +compareNew);
+		
+		System.out.println("=========== Kiem tra vi tri cua Tutorial trong chuoi ===============");
+		int indexTut = text.indexOf("Tutorial");
+		System.out.println("Vi tri cua Tutirial trong chuoi = " + indexTut);
+		
+		System.out.println("=========== Thay the Online bang Offline ===============");
+		String replaceText = text.replace("Online", "Offline");
+		System.out.println("New text = " + replaceText);
+		
+		System.out.println("=========== Dem bao nhieu so xuat hien trong chuoi ===============");
+		int countNum = 0;
+		for (int i =0; i< arrayChar.length ; i++) {
+			boolean verifyNum = Character.isDigit(text.charAt(i));
+			if (verifyNum == true) {
+				countNum++;
+			}
+		} 
+		System.out.println("So ky tu la so trong chuoi = "+ countNum);
+
+		
+	}
+}
